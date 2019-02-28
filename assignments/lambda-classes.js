d1180992c215cd7c5191295637fbb383c1ce4a4f
Stephanie Patton <31425878+StephanieRP@users.noreply.github.com>
2019-02-28 15:51:03 -0600

    completed refactor assignment

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,258 @@
 // CODE here for your Lambda Classes
+
+
+/*
+
+Person
+First we need a Person class. This will be our base-class
+Person receives name age location gender all as props
+Person receives speak as a method.
+This method logs out a phrase Hello my name is Fred, I am from Bedrock where name and location are the object's own props
+
+*/
+
+class Person {
+    constructor(attr) {
+        this.name = attr.name;
+        this.age = attr.age;
+        this.location = attr.location;
+        this.gender = attr.gender;
+    }
+
+    speak() {
+        return `Hello my name is ${this.name}, I am from ${this.location}.`
+    }
+}
+
+/*
+
+Instructor
+Now that we have a Person as our base class, we'll build our Instructor class.
+Instructor uses the same attributes that have been set up by Person
+Instructor has the following unique props:
+specialty what the Instructor is good at i.e. 'redux'
+favLanguage i.e. 'JavaScript, Python, Elm etc.'
+catchPhrase i.e. Don't forget the homies
+Instructor has the following methods:
+demo receives a subject string as an argument and logs out the phrase 'Today we are learning about {subject}' where subject is the param passed in.
+grade receives a student object and a subject string as arguments and logs out '{student.name} receives a perfect score on {subject}'
+
+*/
+
+class Instructor extends Person {
+    constructor(attr) {
+        super(attr);
+        this.specialty = attr.specialty;
+        this.favLanguage = attr.favLanguage;
+        this.catchPhrase = attr.catchPhrase;
+    }
+
+    demo(subject) {
+        return `Today we are learning about ${subject}`;
+    }
+
+    grade(student) {
+       let grade = Math.floor((Math.random() * 100) + 1);
+       student.grade = grade;
+        return `${student.name} receives a score of ${grade}`;
+    }
+
+
+}
+
+/*
+
+Student
+Now we need some students!
+Student uses the same attributes that have been set up by Person
+Student has the following unique props:
+previousBackground i.e. what the Student used to do before Lambda School
+className i.e. CS132
+favSubjects. i.e. an array of the student's favorite subjects ['Html', 'CSS', 'JavaScript']
+Student has the following methods:
+listsSubjects a method that logs out all of the student's favoriteSubjects one by one.
+PRAssignment a method that receives a subject as an argument and logs out that the student.name has submitted a PR for {subject}
+sprintChallenge similar to PRAssignment but logs out student.name has begun sprint challenge on {subject}
+
+
+*/
+
+class Student extends Person {
+    constructor(attr) {
+        super(attr);
+        this.previousBackground = attr.previousBackground;
+        this.favSubjects = attr.favSubjects;
+        this.grade = attr.grade;
+
+    }
+
+    listsSubjects() {
+        return this.favSubjects;
+    }
+
+    prAssignment(subject) {
+        return `${this.name} has submitted a PR for ${subject}.`;
+    }
+
+    sprintChallenge(subject) {
+        return `${this.name} has begun sprint challenge on ${subject}.`;
+    }
+    graduate() {
+        if(this.grade < 70) {
+            return `Sorry ${this.name} you need to get a higher score to graduate.`
+        } else {
+            return `Congrats ${this.name}!!! You are can graduate from Lambda School!!`
+        }
+    }
+}
+
+/*
+
+Project Manager
+Now that we have instructors and students, we'd be nowhere without our PM's
+ProjectManagers are extensions of Instructors
+ProjectManagers have the following unique props:
+gradClassName: i.e. CS1
+favInstructor: i.e. Sean
+ProjectManagers have the following Methods:
+standUp a method that takes in a slack channel and logs `{name} announces to {channel}, @channel standy times!​​​​​
+debugsCode a method that takes in a student object and a subject and logs out {name} debugs {student.name}'s code on {subject}
+
+*/
+
+class ProjectManager extends Instructor {
+    constructor(attr) {
+        super(attr);
+        this.gradClassName = attr.specialty;
+        this.favInstructor = attr.favLanguage;    
+    }
+
+    standUp(slackChannel) {
+        return `${this.name} announces to ${slackChannel} channel, @channel standup time!`;
+    }
+
+    debugsCode(student, subject) {
+        return `${this.name} debugs ${student.name}'s code on ${subject}.`;
+    }
+}
+
+//Objects 
+
+const tom = new Person({
+    name: 'Tom',
+    age: 30,
+    location: 'Los Angeles',
+    gender: 'Male'
+})
+
+const sara = new Person({
+    name: 'Sara',
+    age: 35,
+    location: 'Chicago',
+    gender: 'Female'
+})
+
+const jerry = new Person({
+    name: 'Jerry',
+    age: 22,
+    location: 'New York',
+    gender: 'Male'
+})
+
+// console.log(jerry.speak())
+// console.log(sara.speak())
+// console.log(tom.speak())
+
+const sam = new Instructor({
+    name: 'Sam',
+    age: 41,
+    location: 'New York',
+    gender: 'Male',
+    specialty: 'PHP',
+    catchPhrase: 'Am I right?!'
+})
+
+const olivia = new Instructor({
+    name: 'Olivia',
+    age: 29,
+    location: 'San Francisco',
+    gender: 'Female',
+    specialty: 'Ruby on Rails',
+    catchPhrase: 'That was easy :)'
+})
+
+
+
+const samuel = new Student({
+    name: 'Samuel',
+    age: 19,
+    location: 'Ohio',
+    gender: 'Male',
+    className: 'WEB30',
+    previousBackground: 'Banker',
+    favSubjects: ['JavaScript', 'HTML', 'Machine Learning'],
+    grade: 100
+})
+
+const angel = new Student({
+    name: 'Samuel',
+    age: 33,
+    location: 'Washington',
+    gender: 'Male',
+    className: 'WEB29',
+    previousBackground: 'Consultant',
+    favSubjects: ['Java', 'AI', 'PHP'],
+    grade: 65
+})
+
+const diamond = new Student({
+    name: 'Diamond',
+    age: 22,
+    location: 'Philadelphia',
+    gender: 'Male',
+    className: 'WEB10',
+    previousBackground: 'Accountant',
+    favSubjects: ['CSS', 'AI', 'Python'],
+    grade: 80
+})
+
+// Teachers Test 
+// console.log(sam.speak())
+// console.log(olivia.speak())
+// console.log(sam.demo('PHP'))
+// console.log(olivia.demo('Ruby on Rails'))
+// console.log(sam.specialty)
+// console.log(olivia.catchPhrase)
+// console.log(Sam.grade(diamond, 'PHP'))
+// Students Test 
+// console.log(diamond.speak())
+// console.log(angel.speak())
+// console.log(samuel.listsSubjects())
+// console.log(angel.listsSubjects())
+// console.log(diamond.sprintChallenge('JavaScript'))
+// console.log(samuel.prAssignment('JavaScript'))
+console.log(sam.grade(diamond, 'JavaScript'))
+console.log(diamond.graduate())
+
+const roger = new ProjectManager({
+    name: 'Roger',
+    age: 43,
+    location: 'Springfield',
+    gender: 'Male',
+    gradClassName: 'CS2',
+    favInstructor: 'David'
+})
+
+const gary = new ProjectManager({
+    name: 'Gary',
+    age: 35,
+    location: 'Tennessee',
+    gender: 'Male',
+    gradClassName: 'CS4',
+    favInstructor: 'Howard'
+})
+
+// console.log(roger.speak())
+// console.log(gary.speak())
+// console.log(gary.standUp('Working Hard'))
+// console.log(roger.debugsCode(diamond, 'Javascript'))
--- assignments/prototype-refactor.js
+++ assignments/prototype-refactor.js
@@ -7,3 +7,184 @@ Prototype Refactor
 2. Your goal is to refactor all of this code to use ES6 Classes. The console.log() statements should still return what is expected of them.
 
 */
+
+  class GameObject {
+      constructor(attr) {
+        this.createdAt = attr.createdAt;
+        this.name = attr.name;
+        this.dimensions = attr.dimensions;
+      }
+      destroy() {
+        return `${this.name} was removed from the game.`
+      }
+  }
+  
+
+  class CharacterStats extends GameObject {
+      constructor(attrCharacter) {
+        super(attrCharacter)
+        this.healthPoints = attrCharacter.healthPoints;
+      }
+      takeDamage() {
+        return `${this.name} took damage.`
+      }
+  }
+
+  class Humanoid extends CharacterStats {
+    constructor(attrHumanoid) {
+        super(attrHumanoid)
+    this.team = attrHumanoid.team;
+    this.weapons = attrHumanoid.weapons;
+    this.language = attrHumanoid.language;
+    }
+    greet() {
+        return `${this.name} offers a greeting in ${this.language}`
+      }
+  }
+   
+  /*
+    * Inheritance chain: GameObject -> CharacterStats -> Humanoid
+    * Instances of Humanoid should have all of the same properties as CharacterStats and GameObject.
+    * Instances of CharacterStats should have all of the same properties as GameObject.
+  */
+  
+  // Test you work by un-commenting these 3 objects and the list of console logs below:
+  
+    const mage = new Humanoid({
+      createdAt: new Date(),
+      dimensions: {
+        length: 2,
+        width: 1,
+        height: 1,
+      },
+      healthPoints: 5,
+      name: 'Bruce',
+      team: 'Mage Guild',
+      weapons: [
+        'Staff of Shamalama'
+      ],
+      language: 'Common Tongue',
+    });
+  
+    const swordsman = new Humanoid({
+      createdAt: new Date(),
+      dimensions: {
+        length: 2,
+        width: 2,
+        height: 2,
+      },
+      healthPoints: 15,
+      name: 'Sir Mustachio',
+      team: 'The Round Table',
+      weapons: [
+        'Giant Sword',
+        'Shield',
+      ],
+      language: 'Common Tongue',
+    });
+  
+    const archer = new Humanoid({
+      createdAt: new Date(),
+      dimensions: {
+        length: 1,
+        width: 2,
+        height: 4,
+      },
+      healthPoints: 10,
+      name: 'Lilith',
+      team: 'Forest Kingdom',
+      weapons: [
+        'Bow',
+        'Dagger',
+      ],
+      language: 'Elvish',
+    });
+  
+    console.log(mage.createdAt); // Today's date
+    console.log(archer.dimensions); // { length: 1, width: 2, height: 4 }
+    console.log(swordsman.healthPoints); // 15
+    console.log(mage.name); // Bruce
+    console.log(swordsman.team); // The Round Table
+    console.log(mage.weapons); // Staff of Shamalama
+    console.log(archer.language); // Elvish
+    console.log(archer.greet()); // Lilith offers a greeting in Elvish.
+    console.log(mage.takeDamage()); // Bruce took damage.
+    console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
+  
+  
+    
+
+    // Stretch 
+
+    class Villain extends Humanoid{
+        constructor(attrVillain){
+            super(attrVillain);
+            this.special_skill = attrVillain.special_skill;
+        }
+        villainWinner() {
+            theHero.healthPoints = 0;
+            return `The Hero, ${theHero.name}, has ${theHero.healthPoints} points. ${this.name} is the winner!! he world is doomed!!!`
+          }
+    }
+
+    class Hero extends Humanoid{
+        constructor(attrHero){
+            super(attrHero);
+        }
+        heroWinner () {
+            theVillain.healthPoints = 0;
+            return `The Villain, ${theVillain.name}, has ${theVillain.healthPoints} points. ${this.name} is the winner!! The world is saved!!!`
+          }
+          
+    }
+
+    
+    
+    //// Hero and Villain Objects: 
+    
+    
+      const theVillain = new Villain({
+        createdAt: new Date(),
+        dimensions: {
+          length: 5,
+          width: 2,
+          height: 5,
+        },
+        healthPoints: 1000,
+        name: 'World Ruler',
+        team: 'What\'s a team?',
+        weapons: [
+          'Giant Hammer',
+          'Bare Fist',
+        ],
+        language: 'Fire Dagger',
+      });
+    
+      const theHero = new Hero({
+        createdAt: new Date(),
+        dimensions: {
+          length: 4,
+          width: 3,
+          height: 6,
+        },
+        healthPoints: 1000,
+        name: 'Helper',
+        team: 'Hero\'s Only',
+        weapons: [
+          'Heavy Sword',
+          'Giant Shield',
+        ],
+        language: 'Whisper',
+      });
+
+      console.log(theVillain.createdAt); 
+      console.log(theHero.dimensions); 
+      console.log(theVillain.healthPoints); 
+      console.log(theHero.name);
+      console.log(theHero.team);
+      console.log(theVillain.team);
+      console.log(theVillain.weapons); 
+      console.log(theVillain.language);
+      console.log(theVillain.greet()); 
+      console.log(theVillain.takeDamage());
+      console.log(theHero.heroWinner());
